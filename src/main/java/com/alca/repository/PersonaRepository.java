001c60517c0969cb6074a73076559cf24303e534
Roberto Casana <robcasort@gmail.com>
2022-12-23 16:32:16 -0500

    Se agregan nuevos metodos al repositorio con soporte a Query

--- src/main/java/com/alca/repository/PersonaRepository.java
+++ src/main/java/com/alca/repository/PersonaRepository.java
@@ -1,9 +1,40 @@
 package com.alca.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.alca.model.Persona;
 
+
+// https://docs.spring.io/spring-data/mongodb/docs/1.2.0.RELEASE/reference/html/mongo.repositories.html
+
 public interface PersonaRepository extends MongoRepository<Persona, String> {
+	List<Persona> findByNombre(String nombre);
+	List<Persona> findByApellidos(String apellidos);
 	
+	List<Persona> findByNombreNot(String nombre);
+	List<Persona> findByApellidosNot(String apellidos);
+	
+	List<Persona> findByNombreLike(String nombre);
+	List<Persona> findByApellidosLike(String apellidos);
+	
+	// (Case sensitive)
+	List<Persona> findByNombreRegex(String nombre);
+	List<Persona> findByApellidosRegex(String apellidos);
+	
+	@Query("{'nombre' : ?0}")	
+	List<Persona> buscarPorNombreExacto(String firstname);
+	
+	@Query("{'apellidos' : ?0}")
+	List<Persona> buscarPorApellidosExacto(String apellidos);
+		
+	// (Case sensitive)
+	@Query("{'nombre': {$regex : ?0}})")
+    List<Persona> buscarPorNombreConPatronCaseSentive(String nombre);
+	
+	// Case insesitive
+	@Query("{ 'nombre' : {'$regex' : ?0 , $options: 'i'}}")
+    List<Persona> buscarPorNombreConPatronCaseInsensitive(String patronNombre);
 }
